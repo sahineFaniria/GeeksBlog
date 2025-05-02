ff23325bff21feda9fa6266bec2817095f3beff2
Sahine Faniria <faniriasahine@gmail.com>
2025-05-02 13:43:35 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+ 
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 min-h-svh ">
+      <h1 className="text-7xl font-bold">Here we go</h1>
+      <Button>WebCraft Masters</Button>
+      <p>"Maitres de l'artisanat web".</p>
+    </div>
+  )
+}
+ 
+export default App
